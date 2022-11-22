e98afd549a9293af8caf379a5a986faaee0fc8ee
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-11-22 14:09:20 +0200

    Change name of constants

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,33 +7,33 @@ let finalDl = 0;
 
 const createPromise = (delay, step, position) => {
   console.log(position);
-  const finalDl = delay + step * position;
-  const finPos = position + 1;
+  const finalDelay = delay + step * position;
+  const finalPosition = position + 1;
   setTimeout(() => {
     new Promise(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         return Promise.resolve(
-          `✅ Fulfilled promise ${finPos} in ${finalDl}ms`
+          `✅ Fulfilled promise ${finalPosition} in ${finalDelay}ms`
         ).then(onSuccess => {
           Notify.success(onSuccess);
         });
       }
-      Promise.reject(`❌ Rejected promise ${finPos} in ${finalDl}ms`).catch(
+      Promise.reject(`❌ Rejected promise ${finalPosition} in ${finalDelay}ms`).catch(
         onFailure => {
           Notify.failure(onFailure);
         }
       );
     });
-  }, finalDl);
+  }, finalDelay);
 };
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const delayValue = Number(form.elements[0].value);
   const amountValue = Number(form.elements[2].value);
-  const stepI = Number(form.elements[1].value);
+  const stepValue = Number(form.elements[1].value);
   [...Array(amountValue).keys()].forEach(index =>
-    createPromise(delayValue, stepI, index)
+    createPromise(delayValue, stepValue, index)
   );
 });
