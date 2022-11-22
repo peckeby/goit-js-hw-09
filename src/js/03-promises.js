9fdc57a692e131595c1e527343e83a21822ddda0
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-11-22 13:52:41 +0200

    Format code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,25 +6,34 @@ console.log(form.elements);
 let finalDl = 0;
 
 const createPromise = (delay, step, position) => {
-  console.log(position)
-  const finalDl = delay + (step*position);
-  const finPos = position+1;
+  console.log(position);
+  const finalDl = delay + step * position;
+  const finPos = position + 1;
   setTimeout(() => {
-  new Promise(() => {
-    const shouldResolve = Math.random() > 0.3;
+    new Promise(() => {
+      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-          return Promise.resolve(`✅ Fulfilled promise ${finPos} in ${finalDl}ms`).then(onSuccess =>
-            {Notify.success(onSuccess)})
-    }
-      Promise.reject(`❌ Rejected promise ${finPos} in ${finalDl}ms`).catch(onFailure =>
-          {Notify.failure(onFailure)});
-    })}, finalDl);
-  };
+        return Promise.resolve(
+          `✅ Fulfilled promise ${finPos} in ${finalDl}ms`
+        ).then(onSuccess => {
+          Notify.success(onSuccess);
+        });
+      }
+      Promise.reject(`❌ Rejected promise ${finPos} in ${finalDl}ms`).catch(
+        onFailure => {
+          Notify.failure(onFailure);
+        }
+      );
+    });
+  }, finalDl);
+};
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const delayValue = Number(form.elements[0].value);
   const amountValue = Number(form.elements[2].value);
   const stepI = Number(form.elements[1].value);
-  [...Array(amountValue).keys()].forEach(index => createPromise(delayValue, stepI, index))
-  })
+  [...Array(amountValue).keys()].forEach(index =>
+    createPromise(delayValue, stepI, index)
+  );
+});
